fdb328282e17bee273382af2a4f16b222d813d1a
pmecchia <mecchia.pierre@gmail.com>
2021-04-26 23:32:43 +0200

    start css for players infos

--- webapp/app.py
+++ webapp/app.py
@@ -4,6 +4,9 @@ import pandas as pd
 app = flask.Flask(__name__, template_folder='templates')
 
 df_matches=pd.read_csv('../MatchesDay.csv')
+df_matches['ActualRankingP1']=df_matches.ActualRankingP1.astype(int)
+df_matches['ActualRankingP2']=df_matches.ActualRankingP2.astype(int)
+
 #df_matches=df_matches[["NewNameP1","ActualRankingP1","AvgP1","NewNameP2","ActualRankingP2","AvgP2","PhotoP1","PhotoP2","Hei"]]
 #df_matches.columns = df_matches.iloc[0]
 #df_matches = df_matches[1:]
