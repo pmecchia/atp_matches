c9c7d4b50aee13b4c3e01727997970286fbcb560
pmecchia <mecchia.pierre@gmail.com>
2021-04-29 23:14:40 +0200

    improve css

--- webapp/app.py
+++ webapp/app.py
@@ -7,10 +7,6 @@ df_matches=pd.read_csv('../MatchesDay.csv')
 df_matches['ActualRankingP1']=df_matches.ActualRankingP1.astype(int)
 df_matches['ActualRankingP2']=df_matches.ActualRankingP2.astype(int)
 
-
-#df_matches=df_matches[["NewNameP1","ActualRankingP1","AvgP1","NewNameP2","ActualRankingP2","AvgP2","PhotoP1","PhotoP2","Hei"]]
-#df_matches.columns = df_matches.iloc[0]
-#df_matches = df_matches[1:]
 @app.route('/')
 
 
